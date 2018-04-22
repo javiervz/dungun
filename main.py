284c12b0cefd1a74c38a62f73ba0a78e24749370
javiervz <35106049+javiervz@users.noreply.github.com>
2018-04-22 20:19:26 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 
 from flask import Flask, request, render_template
 from mapu_verbs import verb_to_mapudungun
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -12,12 +13,11 @@ def home():
 
 @app.route('/', methods=["POST"])
 
-verbos=pd.read_csv('verbs.csv',header=0,sep='\t')
-verbos_esp=[verbo for verbo in verbos.esp]
-verbos_mapu=[verbo for verbo in verbos.mapu]
-verbos={esp:mapu for (esp,mapu) in zip(verbos_esp,verbos_mapu)}
-
 def transformed():
+  verbos=pd.read_csv('verbs.csv',header=0,sep='\t')
+  verbos_esp=[verbo for verbo in verbos.esp]
+  verbos_mapu=[verbo for verbo in verbos.mapu]
+  verbos={esp:mapu for (esp,mapu) in zip(verbos_esp,verbos_mapu)}
   text = request.form['text']
   output=verb_to_mapudungun(text,verbos)
   return output#render_template("results.html", output=output)
