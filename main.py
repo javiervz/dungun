0dce059e418e364adf49998a2937d5abf3c6aa81
javiervz <35106049+javiervz@users.noreply.github.com>
2018-04-22 20:10:11 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,6 @@
 from flask import Flask, request, render_template
 from mapu_verbs import verb_to_mapudungun
 
-personas={'singular':{1:'iñche',2:'eymi',3:'fey'},'dual':{1:'iñchiw',2:'eymu',3:'feyengu'},'plural':{1:'iñchiñ',2:'eymün',3:'feyengün'}}
-verbos={'salir':'tripa','decir':'pi','entrar':'kon','tomar mate':'matetu','conversar':'nütramka','subir':'püra','ir':'amu','tomar sopa':'korütu','caminar':'treka','estar':'müle','estudiar':'chillkatu','trabajar':'küdaw','lavar loza':'kücha','despertarse':'trepe','comer pan':'kofketu','barrer':'lepün','bañarse':'müñetu','hablar':'dungu','escuchar':'allkütu','comprar':'ngilla','comer':'inyafütu'}
-consonantes=['n','w']
-
 app = Flask(__name__)
 
 @app.route('/')
@@ -15,9 +11,15 @@ def home():
   return render_template("home.html")
 
 @app.route('/', methods=["POST"])
+
+verbos=pd.read_csv('verbs.csv',header=0,sep='\t')
+verbos_esp=[verbo for verbo in verbos.esp]
+verbos_mapu=[verbo for verbo in verbos.mapu]
+verbos={esp:mapu for (esp,mapu) in zip(verbos_esp,verbos_mapu)}
+
 def transformed():
   text = request.form['text']
-  output=verb_to_mapudungun(text)
+  output=verb_to_mapudungun(text,verbos)
   return output#render_template("results.html", output=output)
 
 if __name__ == '__main__':
