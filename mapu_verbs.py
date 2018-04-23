2897ea1089b8297f9ec9e74646bdd90d730e8d1f
javiervz <35106049+javiervz@users.noreply.github.com>
2018-04-22 23:05:59 -0300

    Update mapu_verbs.py

--- mapu_verbs.py
+++ mapu_verbs.py
@@ -12,36 +12,7 @@
 
 # In[1]:
 
-
 import pandas as pd
-
-
-# ### personas gramaticales
-
-# In[2]:
-
-
-personas={'singular':{1:'iñche',2:'eymi',3:'fey'},'dual':{1:'iñchiw',2:'eymu',3:'feyengu'},'plural':{1:'iñchiñ',2:'eymün',3:'feyengün'}}
-
-
-# ### diccionario de verbos
-
-# In[3]:
-
-
-verbos={'salir':'tripa','decir':'pi','entrar':'kon','tomar mate':'matetu','conversar':'nütramka','subir':'püra','ir':'amu','tomar sopa':'korütu','caminar':'treka','estar':'müle','estudiar':'chillkatu','trabajar':'küdaw','lavar loza':'kücha','despertarse':'trepe','comer pan':'kofketu','barrer':'lepün','bañarse':'müñetu','hablar':'dungu','escuchar':'allkütu','comprar':'ngilla','comer':'inyafütu'}
-
-
-# ### consonantes
-
-# In[4]:
-
-
-consonantes=['n','w']
-
-
-# In[102]:
-
 
 ## verbo en espanol a conjugacion en mapudungun
 verbos=pd.read_csv('verbs.csv',header=0,sep='\t')
