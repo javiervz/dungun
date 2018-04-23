9a2386133731f201c3105e14c006118655a23041
javiervz <35106049+javiervz@users.noreply.github.com>
2018-04-22 23:06:30 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,6 @@ from flask import Flask, request, render_template
 from mapu_verbs import verb_to_mapudungun
 import pandas as pd
 
-personas={'singular':{1:'iñche',2:'eymi',3:'fey'},'dual':{1:'iñchiw',2:'eymu',3:'feyengu'},'plural':{1:'iñchiñ',2:'eymün',3:'feyengün'}}
-verbos={'salir':'tripa','decir':'pi','entrar':'kon','tomar mate':'matetu','conversar':'nütramka','subir':'püra','ir':'amu','tomar sopa':'korütu','caminar':'treka','estar':'müle','estudiar':'chillkatu','trabajar':'küdaw','lavar loza':'kücha','despertarse':'trepe','comer pan':'kofketu','barrer':'lepün','bañarse':'müñetu','hablar':'dungu','escuchar':'allkütu','comprar':'ngilla','comer':'inyafütu'}
-consonantes=['n','w']
-
 app = Flask(__name__)
 
 @app.route('/')
